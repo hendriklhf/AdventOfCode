407589cb8fd96fd52623ed9b3b252ebf52a8e508
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-12-03 21:05:11 +0100

    added benchmarks for day3

--- AdventOfCode.Benchmarks/Benchmarks2022.cs
+++ AdventOfCode.Benchmarks/Benchmarks2022.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode.Year2022.Day1;
 using AdventOfCode.Year2022.Day2;
+using AdventOfCode.Year2022.Day3;
 using BenchmarkDotNet.Attributes;
 
 namespace AdventOfCode.Benchmarks;
@@ -10,6 +11,7 @@ public class Benchmarks2022
 {
     private readonly Puzzle1 _puzzle1 = new();
     private readonly Puzzle2 _puzzle2 = new();
+    private readonly Puzzle3 _puzzle3 = new();
 
     [Benchmark]
     public (uint, uint) Year2022_Day1()
@@ -21,5 +23,17 @@ public class Benchmarks2022
     public (ushort, ushort) Year2022_Day2()
     {
         return _puzzle2.Solve();
+    }
+
+    [Benchmark]
+    public uint Year2022_Day3_Part1()
+    {
+        return _puzzle3.SolvePart1();
+    }
+
+    [Benchmark]
+    public uint Year2022_Day3_Part2()
+    {
+        return _puzzle3.SolvePart2();
     }
 }
