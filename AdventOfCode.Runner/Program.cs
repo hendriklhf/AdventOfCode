18aa1bf9b896ee7627e8297482565010f08f9f66
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-12-06 23:06:36 +0100

    removed hardcoded stacks

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode.Year2022.Day6;
+﻿using AdventOfCode.Year2022.Day5;
 
-Console.WriteLine(new Puzzle6().SolvePart1());
-Console.WriteLine(new Puzzle6().SolvePart2());
+Console.WriteLine(new Puzzle5().SolvePart1());
+Console.WriteLine(new Puzzle5().SolvePart2());
